24e6c643d44997cac88d22e615bc1289967d355c
icoo08217 <icoo08216@gmail.com>
2022-08-04 13:23:57 +0900

    테스트 시 SQL 콘솔 출력

--- src/test/java/com/ll/exam/ArticleServiceTest.java
+++ src/test/java/com/ll/exam/ArticleServiceTest.java
@@ -3,22 +3,32 @@ package com.ll.exam;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.article.service.ArticleService;
 import com.ll.exam.mymap.MyMap;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ArticleServiceTest {
+
+    @BeforeAll
+    public void BeforAll(){
+        MyMap myMap = Container.getObj(MyMap.class);
+
+        // 모든 DB 처리시에, 처리되는 SQL을 콘솔에 출력
+        myMap.setDevMode(true);
+    }
 
     // @BeforeEach를 붙인 아래 메서드는
     // @Test가 달려있는 메서드가 실행되기 전에 자동으로 실행이 된다.
     // 주로 테스트 환경을 깔끔하게 정리하는 역할을 한다.
-    // 즉 각각의 테스트케이스가 독립적인 환경에서 실행될 수 있도록 하는 역할을 한다.
     @BeforeEach
     public void beforeEach() {
         // 게시물 테이블을 깔끔하게 삭제한다.
@@ -109,6 +119,9 @@ public class ArticleServiceTest {
     }
     @Test
     public void modify() {
+
+        // Ut.sleep(5000);
+
         ArticleService articleService = Container.getObj(ArticleService.class);
 
         articleService.modify(1 ,"제목 new" , "내용 new", true);
@@ -118,14 +131,12 @@ public class ArticleServiceTest {
         assertThat(articleDto.getId()).isEqualTo(1);
         assertThat(articleDto.getTitle()).isEqualTo("제목 new");
         assertThat(articleDto.getBody()).isEqualTo("내용 new");
-        assertThat(articleDto.getCreatedDate()).isNotNull();
-        assertThat(articleDto.getModifiedDate()).isNotNull();
         assertThat(articleDto.isBlind()).isEqualTo(true);
 
         // DB에서 받아온 게시물 수정날짜와 자바에서 계산한 현재 날짜를 비교하여(초단위)
         // 그것이 1초 이하로 차이가 난다면
         // 갱신되었다 라고 할 수 있다.
-//        long diffSeconds = ChronoUnit.SECONDS.between(articleDto.getModifiedDate(), LocalDate.now());
-//        assertThat(diffSeconds).isLessThanOrEqualTo(1L);
+        long diffSeconds = ChronoUnit.SECONDS.between(articleDto.getModifiedDate(), LocalDateTime.now());
+        assertThat(diffSeconds).isLessThanOrEqualTo(1L);
     }
 }
