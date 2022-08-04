55afb6cf52c0ae03b7b414dd0ee47f2daa2ff904
icoo08217 <icoo08216@gmail.com>
2022-08-04 13:02:36 +0900

    write 구현

--- src/test/java/com/ll/exam/ArticleServiceTest.java
+++ src/test/java/com/ll/exam/ArticleServiceTest.java
@@ -48,4 +48,19 @@ public class ArticleServiceTest {
     }
 
 
+    @Test
+    public void write() {
+        ArticleService articleService = Container.getObj(ArticleService.class);
+        // selectLong 메서드 이용
+        long newArticleId = articleService.write("제목 new" , "내용 new", false);
+
+        ArticleDto articleDto = articleService.getArticleById(newArticleId);
+
+        assertThat(articleDto.getId()).isEqualTo(newArticleId);
+        assertThat(articleDto.getTitle()).isEqualTo("제목 new");
+        assertThat(articleDto.getBody()).isEqualTo("내용 new");
+        assertThat(articleDto.getCreatedDate()).isNotNull();
+        assertThat(articleDto.getModifiedDate()).isNotNull();
+        assertThat(articleDto.isBlind()).isEqualTo(false);
+    }
 }
